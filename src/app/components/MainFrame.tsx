fb589148ca3c1c474efe46233dee082667b2e9bc
Gehrmann <philipp.gehrmann@BPGDEGUTNB01041.local>
2024-03-06 12:50:37 +0100

    maincommit

--- src/app/components/MainFrame.tsx
+++ src/app/components/MainFrame.tsx
@@ -1,10 +1,23 @@
 "use client";
+import { useRef, useEffect } from "react";
 import { TypeAnimation } from "react-type-animation";
 export default function Mainframe() {
+  const video = useRef(null);
+
+  const playVideo = () => {
+    video.current && video.current.play();
+  };
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      playVideo();
+    }, 1000);
+  });
+
   return (
     <section>
       <div className="grid grid-cols-1 lg:grid-cols-2 h-[100vh] overflow-y-hidden">
-        <div className="text-white grid px-12 pt-16 lg:px-12 lg:pt-0 min-h-[430px] bg-yellow self-center justify-self-start lg:min-h-auto relative z-[150]">
+        <div className="text-white grid px-12 pt-12 lg:px-12 lg:pt-0 min-h-[430px] bg-yellow self-center justify-self-start lg:min-h-auto relative z-[150]">
           <div className="lg:min-w-[500px]">
             <h3 className="text-white ">Ihre Profis für</h3>
             <TypeAnimation
@@ -39,20 +52,14 @@ export default function Mainframe() {
         </div>
 
         <div className="bg-white relative w-full lg:border-l-[1px] lg:border-white z-1 ">
-          <div
-            className="contain absolute left-0 right-0 w-full -translate-y-[300px] lg:-translate-y-[20%]"
-            dangerouslySetInnerHTML={{
-              __html: `
           <video
+            ref={video}
             autoPlay
             muted
             loop
-            style="width:100%;"
-            className="cover"
+            className="cover absolute left-0 right-0 w-full -translate-y-[40%] lg:-translate-y-[20%]"
             src="https://vod-progressive.akamaized.net/exp=1709732346~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F2573%2F21%2F537868903%2F2547815848.mp4~hmac=564956a87b0a01c63ba3478236f8031d539329a758adbc03921c35bc96a523a5/vimeo-prod-skyfire-std-us/01/2573/21/537868903/2547815848.mp4"
-          />`,
-            }}
-          ></div>
+          />
         </div>
       </div>
     </section>
